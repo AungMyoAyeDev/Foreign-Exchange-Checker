3d40ac53dded19d5827a42172bb008450d1eb01f
Aung Myo Aye <aungmyoayedev@gmail.com>
2026-06-15 22:39:30 +0630

    feat : add light and dark mode css value .

--- src/layout/main-layout.tsx
+++ src/layout/main-layout.tsx
@@ -3,8 +3,9 @@ import ThemeToggle from "../components/theme-toggle";
 
 const MainLayout = () => {
   return (
-    <div className="max-w-7xl mx-auto bg-background">
-      <h1>hellow</h1>
+    <div className="max-w-7xl mx-auto h-screen bg-background">
+      <h1 className="  text-preset-1 text-text ">hellow</h1>
+      <div className="w-20 h-12 bg-neutral-900 rounded-4"></div>
       <ThemeToggle />
       <Outlet />
     </div>
